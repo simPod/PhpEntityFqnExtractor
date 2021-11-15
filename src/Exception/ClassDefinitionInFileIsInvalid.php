8c5a74f832c85883aed736e38f1d7b479293e05f
Simon Podlipsky <simon@podlipsky.net>
2021-11-15 22:54:46 +0100

    Catch php-parser exceptions

--- src/Exception/ClassDefinitionInFileIsInvalid.php
+++ src/Exception/ClassDefinitionInFileIsInvalid.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace Cdn77\EntityFqnExtractor\Exception;
 
 use InvalidArgumentException;
+use PhpParser\Error;
 
 use function Safe\sprintf;
 
 final class ClassDefinitionInFileIsInvalid extends InvalidArgumentException
 {
+    public static function cannotParse(string $filePathName, Error $error) : self
+    {
+        return new self(
+            sprintf('Cannot parse file %s', $filePathName),
+            previous: $error
+        );
+    }
+
     public static function noClass(string $filePathName) : self
     {
         return new self(
